493d6a6241a471c09231516cc8cafc3b0a105af8
Javiramos276 <ramosjavier276@gmail.com>
2024-09-21 20:07:33 -0300

    se resuelven todos los ejercicios del punto 2 y algunos del 3

--- Practica5.hs
+++ Practica5.hs
@@ -52,11 +52,56 @@ asegura: { resultado = true ↔ e ∈ s }
 }
 -}
 
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece x (y:ys) | x == y = True
+                   | otherwise = pertenece x ys
+
+{-
+2. todosIguales :: (Eq t) => [t] -> Bool, que dada una lista devuelve verdadero s´ı y solamente s´ı todos sus elementos son iguales
+-}
+
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = True
+todosIguales (x:xs) | x == head xs = todosIguales (tail xs)
+                    | otherwise = False
+
+{-
+3. todosDistintos :: (Eq t) => [t] -> Bool seg´un la siguiente especificaci´on:
+problema todosDistintos (s: seq⟨T⟩) : B {
+requiere: { T rue }
+asegura: { resultado = false ↔ existen dos posiciones distintas de s con igual valor }
+}
+-}
+
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos [_] = True
+todosDistintos (x:xs) | pertenece x xs = False
+                      | otherwise = todosDistintos (head xs:tail xs)
+
+{-
+4. hayRepetidos :: (Eq t) => [t] -> Bool seg´un la siguiente especificaci´on:
+problema hayRepetidos (s: seq⟨T⟩) : B {
+requiere: { True }
+asegura: { resultado = true ↔ existen dos posiciones distintas de s con igual valor }
+}
+-}
+
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos [_] = False
+hayRepetidos (x:xs) | pertenece x xs = True
+                    | otherwise = hayRepetidos (head xs:tail xs)
 
 {-5. quitar :: (Eq t) => t -> [t] -> [t], que dados un entero x y una lista xs, elimina la primera aparici´on de x en
 la lista xs (de haberla).
 -}
 
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar x [] = []
+quitar x (y:ys) | x == y = ys
+                | otherwise = y:quitar x ys
 
 {-
 Resolucion hecha por los profes
@@ -73,7 +118,94 @@ quitarProfes2 n (x:xs) | n == x = xs
                        | otherwise = x:(quitarProfes2 n xs)
 
 {-
+6. quitarTodos :: (Eq t ) => t -> [t] -> [t], que dados un entero x y una lista xs, elimina todas las apariciones
+de x en la lista xs (de haberlas). Es decir:
+problema quitarTodos (e: T, s: seq⟨T⟩) : seq⟨T⟩ {
+requiere: { T rue }
+asegura: { resultado es igual a s pero sin el elemento e. }
+}
 
+-}
+
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos x (y:ys) | pertenece x (y:ys) = quitarTodos x (quitar x (y:ys))
+                     | otherwise = y:ys
+
+{-
+7. eliminarRepetidos :: (Eq t) => [t] -> [t] que deja en la lista una ´unica aparici´on de cada elemento, eliminando
+las repeticiones adicionales.
+-}
+
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) | pertenece x xs = eliminarRepetidos xs  
+                         | otherwise = x : eliminarRepetidos xs --aca no estoy obedeciendo el orden... no creo que importe porque no especifica nada el problema
+
+{-
+8. mismosElementos :: (Eq t) => [t] -> [t] -> Bool, que dadas dos listas devuelve verdadero s´ı y solamente s´ı
+ambas listas contienen los mismos elementos, sin tener en cuenta repeticiones, es decir:
+problema mismosElementos (s: seq⟨T⟩, r: seq⟨T⟩) : B {
+requiere: { T rue }
+asegura: { resultado = true ↔ todo elemento de s pertenece r y viceversa}
+}
+-}
+
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos [] [] = True
+mismosElementos [] _ = False
+mismosElementos (x:xs) (y:ys) | pertenece (head conjuntoA) conjuntoB = mismosElementos (tail conjuntoA) conjuntoB
+                              | otherwise = False
+    where conjuntoA = eliminarRepetidos (x:xs)  
+          conjuntoB = eliminarRepetidos (y:ys)
+
+{-
+Ejercicio 3. Definir las siguientes funciones sobre listas de enteros:
+1. sumatoria :: [Integer] -> Integer seg´un la siguiente especificaci´on:
+problema sumatoria (s: seq⟨Z⟩) : Z {
+requiere: { T rue }
+asegura: { resultado =
+P|s|−1
+i=0 s[i] }
+}
+-}
+
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria l = head l + sumatoria (tail l) 
+
+sumatoria2 :: [Integer] -> Integer
+sumatoria2 [] = 0
+sumatoria2 (x:xs) = x + sumatoria xs 
+
+{-
+2. productoria :: [Integer] -> Integer seg´un la siguiente especificaci´on:
+problema productoria (s: seq⟨Z⟩) : Z {
+requiere: { T rue }
+asegura: { resultado =
+Q|s|−1
+i=0 s[i] }
+}
+-}
+
+productoria :: [Integer] -> Integer
+productoria [] = 1
+productoria (x:xs) = x * productoria xs
+
+{-
+4. sumarN :: Integer -> [Integer] -> [Integer] seg´un la siguiente especificaci´on:
+problema sumarN (n: Z, s: seq⟨Z⟩) : seq⟨Z⟩ {
+requiere: { T rue }
+asegura: {|resultado| = |s| ∧ cada posici´on de resultado contiene el valor que hay en esa posici´on en s sumado
+n }
+}
+-}
+
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN n [] = []
+sumarN n (x:xs) = (n+x): sumarN n xs
+
+{-
 3. maximo :: [Integer] -> Integer seg´un la siguiente especificaci´on:
 problema maximo (s: seq⟨Z⟩) : Z {
 requiere: { |s| > 0 }
